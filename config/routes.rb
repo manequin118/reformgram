1c7ee94acfbc009996dacd338336f0bb393c0518
chu <72890669+manequin118@users.noreply.github.com>
2021-03-24 06:47:33 +0900

    投稿機能view作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,8 @@ Rails.application.routes.draw do
 
   root "pages#home"
   get "/users/:id", to: "users#show", as: "user"
+
+  get "/posts/new", to: "posts#new"
+  post "/posts", to: "posts#create"
+  post "/posts/:post_id/photos", to: "photos#create", as: "post_photos"
 end
