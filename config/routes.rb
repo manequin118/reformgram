a127bd6d0a87e363ab195f27474bd6f1d13770b1
chu <72890669+manequin118@users.noreply.github.com>
2021-03-24 22:19:55 +0900

    投稿一覧の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   devise_for :users,
              controllers: { registrations: "registrations" }
 
-  root "pages#home"
+  root "posts#index"
+
   get "/users/:id", to: "users#show", as: "user"
 
-  get "/posts/new", to: "posts#new"
-  post "/posts", to: "posts#create"
-  post "/posts/:post_id/photos", to: "photos#create", as: "post_photos"
+  resources :posts, only: %i(new create index) do
+    resources :photos, only: %i(create)
+  end
 end
